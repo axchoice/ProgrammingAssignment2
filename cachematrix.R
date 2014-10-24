9c864fc7185375ec730dbbac20ecb1eb33291aa7
axchoice <axchoice@users.noreply.github.com>
2014-10-24 08:37:59 +0800

    Update cachematrix.R
    Comments updated

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,8 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The makeCacheMatrix and cacheSolve functions follow closely the guidelines provided in Assignment 2,
+## minor modifications made. The code is tested with test cases suggested by Gregory D. Horne's hints.  
 
-## Write a short comment describing this function
+## makeCacheMatrix follows the makeVector function in setting up the get, set, getinverse and setinverse functions.
+## Since we are getting the inverse of a matrix, the solve function is used in place of mean in makeVector
 
 makeCacheMatrix <- function(x = matrix()) {
         m <- NULL
@@ -18,7 +19,8 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## cacheSolve follows the cachemean function closely.  
+## Similarly, solve is used here instead of mean in cachemean
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
